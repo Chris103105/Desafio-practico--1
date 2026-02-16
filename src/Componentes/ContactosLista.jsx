90299d5c7dccd8d9648019a02a2ab35bc6c19915
odaly <cruzodaly25@gmail.com>
2026-02-16 00:44:18 -0600

    Terminando componente ContatosLista

--- src/Componentes/ContactosLista.jsx
+++ src/Componentes/ContactosLista.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import Contact from './Contacto';
 
-const ContactList = ({ listaContactos }) => {
+const ContactosLista = ({ listaContactos, borrar, marcarFav }) => {
   return (
     <div className="lista-contenedor">
-      {/* Si la lista está vacía, mostramos mensaje */}
-      {listaContactos.length === 0 ? (
-        <div style={{textAlign: 'center', color: 'white'}}>No hay contactos</div>
-      ) : (
-        /* Si hay datos, los recorremos con .map */
-        listaContactos.map((contacto) => (
-          <Contact key={contacto.id} datos={contacto} />
-        ))
-      )}
+      {listaContactos.map((contacto) => (
+        <Contact 
+          key={contacto.id} 
+          datos={contacto} 
+          borrar={borrar} 
+          marcarFav={marcarFav}
+        />
+      ))}
     </div>
   );
 };
-s
-export default ContactList;
+
+export default ContactosLista;
